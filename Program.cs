d71d37e320b39e69b73915c103ac0795be01c73d
EgeniiM <106548205+EgeniiM@users.noreply.github.com>
2022-06-02 20:57:02 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = ("Egenii ");
+            string surName = ("Maruev");
+            string fio = name + surName;
+            Console.WriteLine("ФИО : {0}",fio);
+            fio = surName + name;
+            Console.WriteLine("ФИО : {0}",fio);
+        }
+    }
+}
